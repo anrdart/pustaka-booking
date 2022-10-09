196536249c449e58f0fe8d190f58261b40d4c484
aluldart <alulcrispy14@gmail.com>
2022-10-09 16:15:33 +0700

    > Added Databases > Added Tables for Data > Added Login Page
    - Fixed Configuration

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('form_validation', 'session', 'database');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('url', 'file');
 
 /*
 | -------------------------------------------------------------------
@@ -120,7 +120,8 @@ $autoload['config'] = array();
 $autoload['language'] = array();
 
 /*
-| -------------------------------------------------------------------
+| ------------------------
+-------------------------------------------
 |  Auto-load Models
 | -------------------------------------------------------------------
 | Prototype:
@@ -132,4 +133,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('ModelUser', 'ModelBuku');
--- application/controllers/latihan.php
+++ application/controllers/latihan.php
@@ -1,17 +1,36 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class latihan extends CI_Controller{
+class Latihan extends CI_Controller
+{
 
     public function __construct()
     {
         parent::__construct();
-        //Codeigniter : Write Less Do More
     }
 
     function index()
     {
-        echo "Saya sedang belajar Web Programming II di kelas 12.3A.35";
+        // echo "Selamat Belajar Web Programing II";
+        $this->load->view('templates/headher');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
+        $this->load->view('tables');
+        $this->load->view('templates/footer');
     }
+    // public function penjumlahan($n1, $n2)
+    // {
 
+    //     $this->load->model('Model_latihan1');
+    //     $data['nilai1'] = $n1;
+    //     $data['nilai2'] = $n2;
+    //     $data['hasil'] = $this->Model_latihan1->jumlah($n1 + $n2);
+
+
+    //     $this->load->view('templates/headher');
+    //     $this->load->view('templates/sidebar');
+    //     $this->load->view('templates/topbar');
+    //     $this->load->view('view_latihan', $data);
+    //     $this->load->view('templates/footer');
+    // }
 }
